1313e8631faa187994bebdfcb1ef9798b28adf10
Mythical-traveler <122392397+Mythical-traveler@users.noreply.github.com>
2025-01-31 12:20:18 -0500

    Sending has been Completed with Host.

--- project/app/src/main/java/project/Host.java
+++ project/app/src/main/java/project/Host.java
@@ -41,11 +41,48 @@ public class Host {
             System.out.println("connected to switch " + switchIP + "at" + switchport);
         }catch (Exception e) {
             System.err.println("Error initializing host: " + e.getMessage());
-            System.exit(1);
+            System.exit(1); 
         }
-        
-
     }
+    private byte[]createFrame(String destMac, String Message){
+        byte[]sourceMacBytes=macAddress.getBytes(StandardCharsets.UTF_8);
+        byte[]destMacBytes=destMac.getBytes(StandardCharsets.UTF_8);
+        byte[]messageBytes=Message.getBytes(StandardCharsets.UTF_8);
+        byte[]frame=new byte[sourceMacBytes.length+destMacBytes.length+messageBytes.length];
+        System.arraycopy(sourceMacBytes,0,frame,0,sourceMacBytes.length);
+        System.arraycopy(destMacBytes,0,frame,sourceMacBytes.length,destMacBytes.length);
+        System.arraycopy(messageBytes, 0, frame, sourceMacBytes.length + destMacBytes.length, messageBytes.length);
+        return frame;
+    }
+
+    private void frameSender(){
+        Scanner scanner=new Scanner(System.in);
+        while(true){
+            System.out.println("Enter Destination MAC ADDRESS: ");
+            String destMac=scanner.nextLine().trim();
+            System.out.println("Enter Message");
+            String Message=scanner.nextLine().trim();
+            byte[]frame=createFrame(destMac, Message);
+            DatagramPacket packet=new DatagramPacket(frame,frame.length,switchIP,switchport);
+            try{
+                socket.send(packet);
+                System.out.println("Sent to: "+ destMac);
+            } catch(Exception e){
+                System.out.println("Error Sending frame! \n Message: "+ e.getMessage());
+            }
+
+        }
+    }
+
+    public static void main(String[] args) {
+        if(args.length!=2){
+            System.out.println("usage: Java Host <HostID> <ConfigFile>");
+            return;
+        }
+        new Host(args[0], args[1]);
+    }
+
+
 
 
 
